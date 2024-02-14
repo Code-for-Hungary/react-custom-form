33467a3b014967c40554f651cd901e077d850045
gazdagergo <info@gazdagergo.com>
2024-02-14 19:21:28 +0100

    Create a basic form closes #1

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,16 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1>Next form page</h1>
+
+        <input type="text" />
+
+        <select>
+          <option>First</option>
+          <option>Second</option>
+          <option>Third</option>
+        </select>
+
+      <input type="checkbox" />
     </main>
   );
 }

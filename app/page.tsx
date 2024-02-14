9c9c5cb592eab3c8d744d09b42742ecb75ed8d5f
gazdagergo <info@gazdagergo.com>
2024-02-14 19:38:45 +0100

    Initial input value handling closes #3

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,24 @@ import {useState} from "react";
 
 export default function Home() {
   const [inputValue, setInputValue] = useState('')
-  const [selectValue, setSelectValue] = useState('')
+  const [selectValue, setSelectValue] = useState('1st')
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1>Next form page</h1>
 
-        <input type="text" onChange={({ target }) => setInputValue(target.value)} />
+        <input
+          type="text" onChange={({ target }) => setInputValue(target.value)}
+          value={inputValue}
+        />
 
-        <select onChange={({ target }) => setSelectValue(target.value)}>
-          <option>First</option>
-          <option>Second</option>
-          <option>Third</option>
+        <select
+          onChange={({ target }) => setSelectValue(target.value)}
+          value={selectValue}
+          >
+          <option value="1st">First</option>
+          <option value="2nd">Second</option>
+          <option value="3rd">Third</option>
         </select>
 
       <input type="checkbox" />
@@ -23,11 +29,11 @@ export default function Home() {
       <table>
         <tbody>
         <tr>
-          <td>input value</td>
+          <td>input value:</td>
           <td>{inputValue}</td>
         </tr>
         <tr>
-          <td>select value</td>
+          <td>select value:</td>
           <td>{selectValue}</td>
         </tr>
         </tbody>

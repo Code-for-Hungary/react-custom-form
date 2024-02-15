f1bad8056fdb8ab96a2fcbcec3555a86c8cc37ab
gazdagergo <info@gazdagergo.com>
2024-02-15 17:22:31 +0100

    Move the state to global state

--- providers/GlobalState.tsx
+++ providers/GlobalState.tsx
@@ -1,11 +1,16 @@
+// @ts-nocheck
 'use client'
 
-import { Context, createContext } from "react";
+import {Context, createContext, useReducer} from "react";
+import {FormInitialState, formInitialState, formReducer} from "@/hooks/useForm";
 
-export const GlobalStateContext: Context<{ state: { foo?: string }}> = createContext({ state: {} })
+export const GlobalStateContext: Context<{ state: FormInitialState, dispatch: (action:{ type: string, payload?: any }) => void }> = createContext({ state: {}, dispatch: ({ type: _, payload: __ }) => null })
+
 const GlobalStateProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(formReducer, formInitialState)
+
   return (
-    <GlobalStateContext.Provider value={{ state: { foo: 'bar' } }}>
+    <GlobalStateContext.Provider value={{ state, dispatch }}>
       {children}
     </GlobalStateContext.Provider>
   )
